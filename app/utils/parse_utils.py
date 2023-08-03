cfccef09111a0e14a19c4a52e16d2ab20281361e
elijah <cuiyeqing960904@gmail.com>
2023-08-03 11:56:56 -0400

    revert

--- app/utils/parse_utils.py
+++ app/utils/parse_utils.py
@@ -5,8 +5,7 @@ from app.errors import DeserializeJSONQueryError, DictKeyError
 
 def get_payload_value(payload: dict, key: str) -> str | dict:
     try:
-        if key != 'db_link':
-            value = payload[key]
+        value = payload[key]
     except KeyError as key_err:
         raise DictKeyError(key, payload) from key_err
     else:
